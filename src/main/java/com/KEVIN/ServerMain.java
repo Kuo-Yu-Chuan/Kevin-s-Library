998f442678eac01a3cd703f19e66bb4bdadeaaf2
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-05-25 17:08:01 +0800

    main

---
+++ src/main/java/com/KEVIN/ServerMain.java
@@ -0,0 +1,7 @@
+package main.java.com.KEVIN;
+
+public class ServerMain {
+    public static void main (String[] args) {
+        //伺服器主程式
+    }
+}
---
+++ src/main/java/com/KEVIN/UserMain.java
@@ -0,0 +1,7 @@
+package main.java.com.KEVIN;
+
+public class UserMain {
+    public static void main (String[] args) {
+        //使用者APP
+    }
+}
