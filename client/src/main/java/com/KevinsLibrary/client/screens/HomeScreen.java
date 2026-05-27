272bde935cda564e89e8b2c268f4e6794f5a7afb
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-05-27 12:46:57 +0800

    Create HomeScreen.java

---
+++ client/src/main/java/com/KevinsLibrary/client/screens/HomeScreen.java
@@ -0,0 +1,65 @@
+package main.java.com.KevinsLibrary.client.screens;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+//主畫面
+public class HomeScreen extends JFrame{
+    public HomeScreen (String userName) {
+
+        JButton loginButton = new JButton ("");
+
+        //視窗名稱
+        if (userName != null) {
+            setTitle ("歡迎來到興老大圖書館 " + userName);
+            loginButton.setText ("切換帳號");
+        }
+        else {
+            setTitle ("歡迎來到興老大圖書館 未登入");
+            loginButton.setText("登入");
+        }
+
+        setSize (1000, 700);    //視窗大小
+        setDefaultCloseOperation (JFrame.DO_NOTHING_ON_CLOSE);    //結束應用程式的X
+        setLocationRelativeTo (null);    //視窗在螢幕正中央
+        setLayout (new FlowLayout (FlowLayout.RIGHT, 10, 40));    //視窗布局
+
+        //新增按鈕
+        loginButton.setFont (new Font ("微軟正黑體", Font.PLAIN, 16));
+        add (loginButton, BorderLayout.NORTH);
+        JButton registerButton = new JButton ("註冊");
+        registerButton.setFont (new Font ("微軟正黑體", Font.PLAIN, 16));
+        add (registerButton, BorderLayout.NORTH);
+        
+        //設定按鈕功能
+        loginButton.addActionListener (new ActionListener () {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                new LoginScreen (userName);
+                dispose ();
+            }
+        });
+        registerButton.addActionListener (new ActionListener () {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                new RegisterScreen (userName);
+                dispose ();
+            }
+        });
+
+        //結束應用程式
+        addWindowListener (new WindowAdapter () {
+            @Override
+            public void windowClosing (WindowEvent e) {
+                //.....這邊要登出.....這邊要登出.....這邊要登出.....這邊要登出.....這邊要登出.....這邊要登出.....這邊要登出.....這邊要登出.....
+                System.exit (0);
+            }
+        });
+        
+        setVisible (true);    //顯示視窗
+    }
+}
