8e5aded5254c3cc006268e6721734fa02bfd1ade
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-06-03 22:38:47 +0800

    Create AdminScreen.java

---
+++ client/src/main/java/com/KevinsLibrary/client/screens/AdminScreen.java
@@ -0,0 +1,14 @@
+package main.java.com.KevinsLibrary.client.screens;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class AdminScreen extends JFrame {
+    public AdminScreen () {
+        
+    }
+}
