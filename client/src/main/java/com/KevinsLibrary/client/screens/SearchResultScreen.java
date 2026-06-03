abc51824cacd88f887b39bbff0ea0996cbee1474
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-06-03 22:24:22 +0800

    Update SearchResultScreen.java

--- client/src/main/java/com/KevinsLibrary/client/screens/SearchResultScreen.java
+++ client/src/main/java/com/KevinsLibrary/client/screens/SearchResultScreen.java
@@ -1,5 +1,6 @@
 package main.java.com.KevinsLibrary.client.screens;
 
+import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,18 +13,21 @@ import main.java.com.KevinsLibrary.Book.*;
 public class SearchResultScreen extends JFrame {
 
     int currentPage = 1;
+    int booksPerPage = 10;
 
-    public SearchResultScreen (Book[] books) {
+    public SearchResultScreen (ArrayList<Book> books) {
 
         setTitle ("興老大圖書館 搜尋結果");    //視窗名稱
         setSize (1200, 700);    //視窗大小
         setDefaultCloseOperation (JFrame.DO_NOTHING_ON_CLOSE);    //結束應用程式的X
         setLocationRelativeTo (null);    //視窗在螢幕正中央
-        Font font = new Font ("微軟正黑體", Font.PLAIN, 14);    //設定字型
+        Font font = new Font ("微軟正黑體", Font.PLAIN, 16);    //設定字型
 
-        if (books != null && books.length != 0) {
-            int pages = (books.length / 5) + (books.length % 5 == 0 ? 0 : 1);
+        if (books != null && books.size () != 0) {
 
+            int pages = (books.size () / booksPerPage) + (books.size () % booksPerPage == 0 ? 0 : 1);
+
+            //翻頁按鈕
             JPanel pagePanel = new JPanel (new FlowLayout ());
             JButton prevPageButton = new JButton ("<");
             JButton nextPageButton = new JButton (">");
@@ -36,15 +40,72 @@ public class SearchResultScreen extends JFrame {
             pagePanel.add (nextPageButton);
             add (pagePanel, BorderLayout.SOUTH);
 
-            //.....這邊要列出.....這邊要列出.....這邊要列出.....這邊要列出.....這邊要列出.....這邊要列出.....這邊要列出.....這邊要列出.....
+            //新增第一頁書目按鈕
+            JPanel listPanel = new JPanel (new GridBagLayout ());
+            JButton titleButtons[] = new JButton [booksPerPage];
+            JLabel authorLabels[] = new JLabel [booksPerPage];
+            JLabel yearLabels[] = new JLabel [booksPerPage];
+            GridBagConstraints gbc = new GridBagConstraints ();    //控制位置
+            int vGap = 15;
+            gbc.insets = new Insets (vGap / 2, 10, vGap/2, 10);    //上左下右的空間
+            int firstNumOfBooks = books.size () < booksPerPage ? books.size () : booksPerPage;    //避免超出陣列範圍
+            for (int i = 0; i < firstNumOfBooks; i++) {
+
+                Book currentBook = books.get (booksPerPage * currentPage - booksPerPage + i);
+
+                //書名按鈕
+                titleButtons[i] = new JButton (currentBook.getTitle ());
+                titleButtons[i].setFont (font);
+                titleButtons[i].setHorizontalAlignment (SwingConstants.LEFT);    //靠左
+                gbc.fill = GridBagConstraints.HORIZONTAL;    //書名太長時會變成...
+                gbc.gridx = 0; gbc.gridy = i;    //[i][0]的位置
+                gbc.weightx = 0.6;    //長度佔60%
+                listPanel.add (titleButtons[i], gbc);
+
+                //作者文字
+                authorLabels[i] = new JLabel ("作者：" + currentBook.getAuthor ());
+                authorLabels[i].setFont (font);
+                gbc.gridx = 1;
+                gbc.weightx = 0.2;
+                listPanel.add (authorLabels[i], gbc);
+
+                //出版年份文字
+                yearLabels[i] = new JLabel ("出版年份：" + Integer.toString (currentBook.getYear ()));
+                yearLabels[i].setFont (font);
+                gbc.gridx = 2;
+                gbc.weightx = 0.2;
+                listPanel.add (yearLabels[i], gbc);
+            }
+            add (listPanel);
 
             //設定按鈕功能
+            for (int i = 0; i < firstNumOfBooks; i++) {
+                int temp = i;    //就是要用，不然有錯。我也不知道為什麼。
+                titleButtons[temp].addActionListener (new ActionListener () {
+                    @Override
+                    public void actionPerformed (ActionEvent e) {
+                        new BookScreen (books.get (booksPerPage * currentPage - booksPerPage + temp));
+                    }
+                });
+            }
             prevPageButton.addActionListener (new ActionListener () {
                 @Override
                 public void actionPerformed (ActionEvent e) {
                     if (currentPage != 1) {
                         currentPage--;
                         pageLabel.setText ("Page " + Integer.toString (currentPage) + "/" + Integer.toString (pages));
+
+                        for (int i = 0; i < booksPerPage; i++) {
+                            titleButtons[i].setVisible (true);
+                            authorLabels[i].setVisible (true);
+                            yearLabels[i].setVisible (true);
+
+                            Book currentBook = books.get (booksPerPage * currentPage - booksPerPage + i);
+                            titleButtons[i].setText (currentBook.getTitle ());
+                            authorLabels[i].setText ("作者：" + currentBook.getAuthor ());
+                            yearLabels[i].setText ("出版年份：" + Integer.toString (currentBook.getYear ()));
+                        }
+                        revalidate ();  repaint ();
                     }
                 }
             });
@@ -54,6 +115,23 @@ public class SearchResultScreen extends JFrame {
                     if (currentPage != pages) {
                         currentPage++;
                         pageLabel.setText ("Page " + Integer.toString (currentPage) + "/" + Integer.toString (pages));
+                        
+                        int numOfBooks = booksPerPage;
+                        if (currentPage == pages - 1) {
+                            numOfBooks = books.size () % booksPerPage;
+                            for (int i = numOfBooks; i < booksPerPage; i++) {
+                                titleButtons[i].setVisible (false);
+                                authorLabels[i].setVisible (false);
+                                yearLabels[i].setVisible (false);
+                            }
+                        }
+                        for (int i = 0; i < numOfBooks; i++) {
+                            Book currentBook = books.get (booksPerPage * currentPage - booksPerPage + i);
+                            titleButtons[i].setText (currentBook.getTitle ());
+                            authorLabels[i].setText ("作者：" + currentBook.getAuthor ());
+                            yearLabels[i].setText ("出版年份：" + Integer.toString (currentBook.getYear ()));
+                        }
+                        revalidate ();  repaint ();
                     }
                 }
             });
