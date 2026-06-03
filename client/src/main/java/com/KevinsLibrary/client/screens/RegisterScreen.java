91fdb2452cace619e5fc00d36b98a41b12a1299b
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-06-03 15:45:16 +0800

    Update RegisterScreen.java

--- client/src/main/java/com/KevinsLibrary/client/screens/RegisterScreen.java
+++ client/src/main/java/com/KevinsLibrary/client/screens/RegisterScreen.java
@@ -7,9 +7,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import main.java.com.KevinsLibrary.userType.User;
+
 //註冊畫面
 public class RegisterScreen extends JFrame {
-    public RegisterScreen (String userName) {
+    public RegisterScreen (User user) {
         
         setTitle ("興老大圖書館 註冊");    //視窗名稱
         setSize (700, 500);    //視窗大小
@@ -30,7 +32,6 @@ public class RegisterScreen extends JFrame {
         passwordLabel.setFont (font);
         JLabel nameLabel = new JLabel ("使用者名稱：");
         nameLabel.setFont (font);
-        JLabel idLabel = new JLabel ("身分證字號：");
         JLabel failLabel = new JLabel ("", SwingConstants.CENTER);
         failLabel.setForeground (Color.RED);
         failLabel.setFont (font);
@@ -42,8 +43,6 @@ public class RegisterScreen extends JFrame {
         passwordField.setFont (font);
         JTextField nameField = new JTextField ();
         nameField.setFont (font);
-        JTextField idField = new JTextField ();
-        idField.setFont (font);
 
         //Panel 用於整合 Label 、 Field 、 Botton
         JPanel inputPanel = new JPanel (new GridLayout (4, 2, 10, 10));
@@ -53,8 +52,6 @@ public class RegisterScreen extends JFrame {
         inputPanel.add (passwordField);
         inputPanel.add (nameLabel);
         inputPanel.add (nameField);
-        inputPanel.add (idLabel);
-        inputPanel.add (idField);
         JPanel buttonPanel = new JPanel (new GridLayout (2, 1, 5, 5));
         buttonPanel.add (registerButton);
         buttonPanel.add (cancelButton);
@@ -71,15 +68,14 @@ public class RegisterScreen extends JFrame {
         registerButton.addActionListener (new ActionListener () {
             @Override
             public void actionPerformed (ActionEvent e) {
-                String userName = accountField.getText ().trim ();
+                String userID = accountField.getText ().trim ();
                 String password = new String (passwordField.getPassword ());
-                //String name = nameField.getText ().trim ();
-                //String id = new String (idField.getPassword ());
+                String UserName = nameField.getText ().trim ();
 
                 //.....這邊要檢查.....這邊要檢查.....這邊要檢查.....這邊要檢查.....這邊要檢查.....這邊要檢查.....這邊要檢查.....這邊要檢查.....
                 //.....這邊要登入.....這邊要登入.....這邊要登入.....這邊要登入.....這邊要登入.....這邊要登入.....這邊要登入.....這邊要登入.....
-                if (userName.equals("fuck") && password.equals("1234")) {
-                    new HomeScreen (userName);
+                if (userID.equals("fuck") && password.equals("1234")) {
+                    new HomeScreen (user);
                     dispose();
                 } else {
                     failLabel.setText("不讓你註冊！");
@@ -89,7 +85,7 @@ public class RegisterScreen extends JFrame {
         cancelButton.addActionListener (new ActionListener () {
             @Override
             public void actionPerformed (ActionEvent e) {
-                new HomeScreen (userName);
+                new HomeScreen (user);
                 dispose ();
             }
         });
@@ -98,7 +94,7 @@ public class RegisterScreen extends JFrame {
         addWindowListener (new WindowAdapter () {
             @Override
             public void windowClosing (WindowEvent e) {
-                new HomeScreen (userName);
+                new HomeScreen (user);
                 dispose ();
             }
         });
