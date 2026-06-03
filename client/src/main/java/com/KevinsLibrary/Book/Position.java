e3688844e0750bf4b4a4e3a937294b9305b4c6dc
Kuo-Yu-Chuan <rexkuo950712@gmail.com>
2026-06-03 22:24:11 +0800

    Update Position.java

--- client/src/main/java/com/KevinsLibrary/Book/Position.java
+++ client/src/main/java/com/KevinsLibrary/Book/Position.java
@@ -4,17 +4,37 @@ public class Position {
 
     private byte library;
     private byte floor;
+    private String area;
 
-    public Position(byte library, byte floor) {
+    public Position(byte library, byte floor, String area) {
         this.library = library;
         this.floor = floor;
+        this.area = area;
     }
 
     public byte getLibrary() {
         return library;
     }
 
+    public String getLibraryString () {
+        switch (library) {
+            case 1 : return "興老大一館";
+            case 2 : return "興老大二館";
+            case 3 : return "興老大三館";
+            case 4 : return "興老大四館";
+            default : return "興老大總館";
+        }
+    }
+
     public byte getFloor() {
         return floor;
+    }
+
+    public String getFloorString () {
+        return " " + Byte.toString (floor) + "F ";
+    }
+
+    public String getArea () {
+        return area;
     }
 }
